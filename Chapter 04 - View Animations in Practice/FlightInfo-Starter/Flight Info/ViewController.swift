3d387172872ef6d69b8fb792b2760c3ccb79fe01
yaoyt <yingtaoyao@creditease.cn>
2017-03-28 15:48:22 +0800

    Chapter04 finished

--- Chapter 04 - View Animations in Practice/FlightInfo-Starter/Flight Info/ViewController.swift
+++ Chapter 04 - View Animations in Practice/FlightInfo-Starter/Flight Info/ViewController.swift
@@ -26,12 +26,17 @@ import QuartzCore
 //
 // Util delay function
 //
-func delay(seconds seconds: Double, completion:()->()) {
-  let popTime = dispatch_time(DISPATCH_TIME_NOW, Int64( Double(NSEC_PER_SEC) * seconds ))
+func delay(seconds: Double, completion:@escaping ()->()) {
+  let popTime = DispatchTime.now() + Double(Int64( Double(NSEC_PER_SEC) * seconds )) / Double(NSEC_PER_SEC)
   
-  dispatch_after(popTime, dispatch_get_main_queue()) {
+  DispatchQueue.main.asyncAfter(deadline: popTime) {
     completion()
   }
+}
+
+enum AnimationDirection:Int {
+    case Positive = 1;
+    case Negative = -1;
 }
 
 class ViewController: UIViewController {
@@ -63,34 +68,116 @@ class ViewController: UIViewController {
     
     //add the snow effect layer
     snowView = SnowView(frame: CGRect(x: -150, y:-100, width: 300, height: 50))
-    let snowClipView = UIView(frame: CGRectOffset(view.frame, 0, 50))
+    let snowClipView = UIView(frame: view.frame.offsetBy(dx: 0, dy: 50))
     snowClipView.clipsToBounds = true
     snowClipView.addSubview(snowView)
     view.addSubview(snowClipView)
     
     //start rotating the flights
-    changeFlightDataTo(londonToParis)
+    changeFlightDataTo(londonToParis,animated: true)
   }
   
   //MARK: custom methods
   
-  func changeFlightDataTo(data: FlightData) {
+  func changeFlightDataTo(_ data: FlightData, animated: Bool = false) {
+    
+    if animated {
+        self.fadeImageView(imageView: bgImageView, toImage: UIImage(named: data.weatherImageName)!, showEffects: data.showWeatherEffects);
+        
+        let direction:AnimationDirection = data.isTakingOff ? .Positive : .Negative;
+        cubeTransition(label: flightNr, text: data.flightNr, direction: direction);
+        cubeTransition(label: gateNr , text: data.gateNr, direction: direction);
+        
+        let offsetDeparting = CGPoint(x: CGFloat(direction.rawValue * 80), y: 0.0)
+        moveLabel(label: departingFrom, text: data.departingFrom, offset: offsetDeparting)
+        let offsetArriving = CGPoint( x: 0.0, y: CGFloat(direction.rawValue * 50))
+        moveLabel(label: arrivingTo, text: data.arrivingTo, offset: offsetArriving)
+
+    } else {
+        bgImageView.image = UIImage(named: data.weatherImageName)
+        snowView.isHidden = !data.showWeatherEffects
+        flightNr.text = data.flightNr
+        gateNr.text = data.gateNr
+        departingFrom.text = data.departingFrom
+        arrivingTo.text = data.arrivingTo
+    }
     
     // populate the UI with the next flight's data
     summary.text = data.summary
-    flightNr.text = data.flightNr
-    gateNr.text = data.gateNr
-    departingFrom.text = data.departingFrom
-    arrivingTo.text = data.arrivingTo
+
+
     flightStatus.text = data.flightStatus
-    bgImageView.image = UIImage(named: data.weatherImageName)
-    snowView.hidden = !data.showWeatherEffects
+
     
     // schedule next flight
     delay(seconds: 3.0) {
-      self.changeFlightDataTo(data.isTakingOff ? parisToRome : londonToParis)
+      self.changeFlightDataTo(data.isTakingOff ? parisToRome : londonToParis,animated: true)
     }
   }
+    
+    func fadeImageView(imageView: UIImageView, toImage: UIImage, showEffects: Bool) {
+        
+        UIView.transition(with: imageView, duration: 1.0, options: [.transitionCrossDissolve], animations: {
+            imageView.image = toImage;
+        }, completion: nil);
+        
+        UIView.animate(withDuration: 1.0, delay: 0.0, options: [.curveEaseOut], animations: {
+            self.snowView.alpha = showEffects ? 1.0 : 0.0;
+        }, completion: nil);
+    }
+    
+    func cubeTransition(label: UILabel, text: String, direction: AnimationDirection) {
+        let tempLabel = UILabel(frame: label.frame);
+        tempLabel.text = text;
+        tempLabel.font = label.font;
+        tempLabel.backgroundColor = label.backgroundColor;
+        tempLabel.textColor = label.textColor;
+        tempLabel.textAlignment = label.textAlignment;
+        
+        let offset = CGFloat(direction.rawValue) * label.bounds.height/2;
+        let tranform = CGAffineTransform(scaleX: 1.0, y: 0.1).concatenating(CGAffineTransform(translationX: 0.0, y: offset));
+        tempLabel.transform = tranform;
+        
+        label.superview?.addSubview(tempLabel);
+        
+        UIView.animate(withDuration: 1.0, delay: 0, options: [], animations: { 
+            tempLabel.transform = CGAffineTransform.identity;
+            let tranform1 = CGAffineTransform(scaleX: 1.0, y: 0.1).concatenating(CGAffineTransform(translationX: 0.0, y: -offset));
+            label.transform = tranform1;
+        }, completion: {_ in
+            label.text = tempLabel.text;
+            label.transform = CGAffineTransform.identity;
+            tempLabel.removeFromSuperview();
+        });
+    }
+    
+    func moveLabel(label: UILabel, text: String, offset: CGPoint) {
+        let auxLabel = UILabel(frame: label.frame)
+        auxLabel.text = text
+        auxLabel.font = label.font
+        auxLabel.textAlignment = label.textAlignment
+        auxLabel.textColor = label.textColor
+        auxLabel.backgroundColor = UIColor.clear
+        auxLabel.transform = CGAffineTransform( translationX: offset.x, y: offset.y)
+        auxLabel.alpha = 0
+        view.addSubview(auxLabel)
+        
+        UIView.animate(withDuration: 0.5, delay: 0, options: [.curveEaseIn], animations: { 
+            label.transform = CGAffineTransform( translationX: offset.x, y: offset.y)
+            label.alpha = 0.0
+        }, completion: nil);
+        
+        UIView.animate(withDuration: 0.25, delay: 0.1, options: [.curveEaseIn], animations: { 
+            auxLabel.transform = CGAffineTransform.identity;
+            auxLabel.alpha = 1.0;
+        }) { _ in
+            label.text = text;
+            label.transform = CGAffineTransform.identity;
+            label.alpha = 1.0;
+            auxLabel.removeFromSuperview();
+        }
+     
+    }
   
   
 }
